9ad436aec3195413da2c2d705a41195c9f070d14
motasimbabiker <82777631+motasimbabiker@users.noreply.github.com>
2021-04-30 23:34:17 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const input=prompt("inter the number")
+ const value = Number(input) || 0
+console.log(value);
+const input2=prompt("inter the nuber2")
+const value2 = Number(input2) || 0
+console.log(input2);
+console.log(value + value2);
+
+
+/*
+const input2=prompt("inter the nuber2")
+input2 = null
+value2 = Number(null) || 0
+value2 = 0 || 0
+value2 = Boolean(0)|| 0
+value2 = 0
+
+
+*/
